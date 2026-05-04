422a702837283c30fc92be1214fa0cecbf5c21ce
Nicole <idnicoletal@gmail.com>
2026-05-04 23:19:31 +0000

    fix: move Google Fonts from CSS @import to layout <link> tags to fix sucrase parse error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;700&family=Nunito:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400&family=Jost:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
       </head>
       <body className="bg-tgol-cream min-h-screen font-futura">
         <GameProvider>
